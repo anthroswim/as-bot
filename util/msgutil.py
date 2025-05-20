453b2988dfb287bbc877a47ea14b0992d80369e5
Kovács <balintkovacs11@gmail.com>
2025-05-20 11:34:20 +0200

    add text to reddit image post body

--- util/msgutil.py
+++ util/msgutil.py
@@ -43,7 +43,7 @@ def escape_markdown_extra(text: str, unembed_liks = False) -> str:
     text = escape_markdown(text)
 
     if unembed_liks:
-        text = re.sub(r"<(https?://\S+)>", r"\1", text)
+        text = re.sub(r"(https?://\S+)", r"<\1>", text)
     
     return text
 
